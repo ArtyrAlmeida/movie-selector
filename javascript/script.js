126445e92bf62d00e7ea35ebf167a1b74c22991e
Artyr Almeida <arturoalmeidafreire@gmail.com>
2021-11-07 00:45:44 -0300

    Funções básicas concluídas

--- javascript/script.js
+++ javascript/script.js
@@ -7,17 +7,53 @@ let resultPage = document.getElementById("resultMain");
 let result = document.getElementById("result");
 let movies = new Array();
 
+let movie0 = document.getElementById("movie0");
+let movie1 = document.getElementById("movie1");
+let movie2 = document.getElementById("movie2");
+let movie3 = document.getElementById("movie3");
+let movie4 = document.getElementById("movie4");
+let movie5 = document.getElementById("movie5");
+let movie6 = document.getElementById("movie6");
+
+let movieDisplays = new Array(movie0, movie1, movie2, movie3, movie4, movie5, movie6);
+
 addButton.addEventListener("click", addMovie);
 sortButton.addEventListener("click", sortMovie);
 changeButton.addEventListener("click", changeMovies);
 
 function addMovie(){
-    movies.push(valorInput.value);
+    if(movies.length<=6){
+        movies.push(valorInput.value);
+        valorInput.value="Insira um filme";
+    }
+    else{
+        valorInput.value="Máximo de filmes atingido";
+        valorInput.style.color="red"; 
+    }
+
+    showMovie();
+}
+
+function removeMovie(i){
+    movies.splice(i, 1);
+
+    showMovie();
+
     valorInput.value="Insira um filme";
+    valorInput.style.color="white"; 
 }
 
 function showMovie(){
-    
+    let i;
+
+    for(i=0;i<movieDisplays.length;i++){
+        movieDisplays[i].innerHTML=" ";
+
+    }
+
+    for(i=0;i<movies.length;i++){
+        movieDisplays[i].innerHTML=movies[i];
+    }
 }
 
 function sortMovie(){
